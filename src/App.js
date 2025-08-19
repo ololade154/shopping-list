662c42433caf880b11bb90f04ac79be55ab3c6a7
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-08-19 12:53:55 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-const lists = [
-  {
-    id: 1,
-    name: 'Bread',
-  },
-  {
-    id: 2,
-    name: 'Sugar',
-  },
-  {
-    id: 3,
-    name: 'Beans',
-  },
-];
+// const lists = [
+//   {
+//     id: 1,
+//     name: 'Bread',
+//   },
+//   {
+//     id: 2,
+//     name: 'Sugar',
+//   },
+//   {
+//     id: 3,
+//     name: 'Beans',
+//   },
+// ];
+import { useState } from 'react';
 function App() {
   return (
     <div className="App">
@@ -20,73 +21,93 @@ function App() {
   );
 }
 function Container() {
+  const [items, setItems] = useState([]);
   return (
     <div className="shopping-list">
       <Header />
-      <Content />
-      <ShoppingList />
+      <Content setItems={setItems} />
+      <ShoppingList lists={items} />
     </div>
   );
 }
 function Header() {
   return <h1 className="header">Shopping List</h1>;
 }
-function Content() {
+function Content({ setItems }) {
+  const [title, setTitle] = useState('');
+  const [id, setId] = useState('');
+  const addToList = () => {
+    console.log(title, id, 'id');
+    setItems((prev) => [...prev, { title, id }]);
+    setId('');
+    setTitle('');
+  };
   return (
     <div className="content">
-      <InputField placeholder="Title..." />
-      <InputField width="40px" placeholder="14" />
+      <input
+        className="input-field"
+        placeholder="Title"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        style={{ width: '90px' }}
+      />
+      <input
+        className="input-field"
+        placeholder="14"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+        type="text"
+        style={{ width: '90px' }}
+      />
       <Button
+        onClick={addToList}
         backgroundColor="transparent"
         border=" 1px solid #FFD700"
         color="#FFD700"
-        padding="5px 4px"
+        padding="5px 10px"
       >
         Add
       </Button>
     </div>
   );
 }
-function InputField({ placeholder, width = '190px' }) {
-  return (
-    <div>
-      <input
-        className="input-field"
-        placeholder={placeholder}
-        type="text"
-        style={{ width }}
-      ></input>
-    </div>
-  );
-}
+
 function Button({
   children,
   backgroundColor,
-  width = '50px',
+  width = '70px',
   border,
   color,
   padding,
+  onClick,
 }) {
   return (
     <button
       className="button"
+      onClick={onClick}
       style={{ backgroundColor, width, border, color, padding }}
     >
       {children}
     </button>
   );
 }
-function ShoppingList() {
+function ShoppingList({ lists }) {
   return (
-    <ul>
+    <div>
       {lists.map((list) => (
-        <List name={list.name} key={list.id} />
+        <List id={list.id} name={list.name} key={list.id} />
       ))}
-    </ul>
+    </div>
   );
 }
-function List({ name }) {
-  return <li className="list">{name}</li>;
+function List({ name, id }) {
+  return (
+    <div className="list">
+      <p>{name}</p>
+      <button onClick={() => console.log(id, 'id')}>X</button>
+    </div>
+  );
 }
 
 export default App;
